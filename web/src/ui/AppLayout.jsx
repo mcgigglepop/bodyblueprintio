01ce52da23265bbb1f53ad7e39e8a909af0a798f
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-11 21:30:58 -0600

    added styles

--- web/src/ui/AppLayout.jsx
+++ web/src/ui/AppLayout.jsx
@@ -1,16 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../pages/Sidebar";
 import Header from "./Header";
+import styled from "styled-components";
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  padding: 4rem 4.8rem 6.4rem;
+`;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Sidebar />
       <Header />
-      <main>
+      <Main>
         <Outlet />
-      </main>
-    </div>
+      </Main>
+    </StyledAppLayout>
   );
 }
 
--- web/src/ui/Header.jsx
+++ web/src/ui/Header.jsx
@@ -1,8 +1,16 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: var(--color-grey-0);
+  padding: 1.2rem 4.8rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+
 function Header() {
   return (
-    <header>
+    <StyledHeader>
       Header
-    </header>
+    </StyledHeader>
   );
 }
 
