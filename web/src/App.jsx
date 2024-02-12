1699bbe9105f62cf2fafde24f14dd2fffcdbacc6
cristopher ontiveros <christoph.alano@gmail.com>
2024-02-11 20:45:31 -0600

    added global style

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
 function App() {
-  return <div>Hello world</div>
+  return (
+    <>
+      <GlobalStyles />
+      <div>
+        <H1>Body Blueprint</H1>
+    </div>
+    </>
+  );
 }
 
 export default App;
